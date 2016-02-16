63ce4d176db460dc03a5e139520503bc043b25f1
JJ Espinoza <espin086@gmail.com>
2016-02-15 16:14:06 -0800

    near final version of regression diagnostics

--- Regression Diagnostics.R
+++ Regression Diagnostics.R
@@ -1,29 +1,51 @@
-library(sandwich)
-data("PublicSchools")
-summary(PublicSchools)
+library(car)
+library(datasets)
 
-#Removing school with missing data, execise caution with removing missing data
-ps <- na.omit(PublicSchools)
-ps$Income <- ps$Income / 10000
+fit <- lm(Fertility ~ ., data=swiss)
 
-#Plot the data to examine if there influential observations
-plot(Expenditure ~ Income, data = ps, ylim = c(230, 830))
-ps_lm <- lm(Expenditure ~ Income, data = ps)
-abline(ps_lm)
+############################################
+#function for ols diagnostics
+ols.diagnostics <- function(ols.model){
+        
+        #############################################
+        #The basic tool for diagnosing regression models are residual plots
+        plot(ols.model, which = 1:6)
+        
+        #############################################
+        #Deletion diagnostics is another tool to examine influential observations
+        print("#############################################")
+        print(summary(influence.measures(ols.model)))
+        
+        print("#############################################")
+        print("TYPE:Testing for heteroskedasticity:BP test")
+        print("INTERPRETATION: if p-value less than 0.05 then heteroskedasticity exists")
+        print("#############################################")
+        library(lmtest)
+        print(bptest(ols.model))
+        
+        print("#############################################")
+        print("TYPE: Testing for functional misspecification; RESET TEST") 
+        print("INTERPRETATION: if p-value > 0.05 then no mispecificaiton")
+        print("#############################################")
+        print(resettest(ols.model))
+  
+        print("#############################################")
+        print("TYPE: Multicollinearity Test; VIF") 
+        print("INTERPRETATION: if greater than 10 variable is multicollinear")      
+        print("#############################################")
+        print(vif(fit))
+        
+        print("#############################################")
+        print("TYPE: Non-Linearity Test; Added Variable Plots") 
+        print("INTERPRETATION: conditional regression plots for non-linearity")      
+        print("#############################################")
+        avPlots(fit)
+        
+        print("#############################################")
+        print("REGRESSION MODEL") 
+        print("#############################################")
+        summary(fit)
+}
 
-#############################################
-#The basic tool for diagnosing regression models are residual plots
-plot(ps_lm, which = 1:6)
-
-#The first plot is to examine if there are any systematic variations in the data
-#The second plot test the assumptions behind the statistical tests and confidence intervals assume the errors are i.i.d and normally distributed
-#The third plot checks for homoskedasticity which can also impact statistical significance tests and also checks i.i.d. errors
-#The fourth plot shows Cook's distance, a statistical measure of being an outlier
-
-
-#############################################
-#Deletion diagnostics is another tool to examine influential observations
-influence.list <- influence.measures(ps_lm)
-summary(influence.list)
-influence.stat <- as.data.frame(influence.list[[1]])
-influence.bol <- as.data.frame(influence.list[[2]])
+#Calling the function
+ols.diagnostics(fit)
